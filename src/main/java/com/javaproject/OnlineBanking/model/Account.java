6e7b6e456b4deed643935e909a3b78ceba867a81
Ioannis Laventzakis <glaventzakis@gmail.com>
2024-06-03 09:29:16 +0200

    updated

--- src/main/java/com/javaproject/OnlineBanking/model/Account.java
+++ src/main/java/com/javaproject/OnlineBanking/model/Account.java
@@ -2,14 +2,11 @@ package com.javaproject.OnlineBanking.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
+
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "bank_entries")
 @Entity
 public class Account {
@@ -22,11 +19,55 @@ public class Account {
 
     @Column(name = "account_number",unique = true,nullable = false)
     private String accountNumber;
-
-    @Column(nullable = false)
+    private String accountType;
     private double balance;
 
-  @Column(name= "account_type", nullable = false)
-private String accountType;
+  // Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Account() {
+    }
+
+    public Account(Long id, String accountNumber, String accountType, double balance) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+    public Account(String accountNumber, String accountType, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
 
 }
